d9bbe5ceb55dad9cca6c2e935ada8a0f96efa378
Matteo Corain <m96.corain@gmail.com>
2020-01-07 09:44:50 +0100

    Dense cells computation corrected, commenting

--- src/main/java/it/polito/s256654/thesis/OutlierDetector.java
+++ src/main/java/it/polito/s256654/thesis/OutlierDetector.java
@@ -64,13 +64,16 @@ public class OutlierDetector implements Serializable {
      * Creates the 2D grid of points with diagonal eps.
      * 
      * @param dataset The RDD containing all points.
-     * @return A PairRDD containing, for all cell, the list of points.
+     * @return A PairRDD containing, for all cell, the corresponding points.
      */
     private JavaPairRDD<Cell, Vector> createGrid(JavaRDD<Vector> dataset) {
         JavaPairRDD<Cell, Vector> allCells = dataset
             .mapToPair(p -> {
+                /* Compute cell coordinates */
                 int cellX = (int) (p.apply(0) / eps);
                 int cellY = (int) (p.apply(1) / eps);
+
+                /* Emit a pair (cell, point) */
                 return new Tuple2<>(new Cell(cellX, cellY), p);
             })
             .cache();
@@ -86,10 +89,11 @@ public class OutlierDetector implements Serializable {
      */
     private JavaPairRDD<Cell, Vector> getDenseCells(JavaPairRDD<Cell, Vector> allCells) {
         JavaPairRDD<Cell, Vector> denseCells = allCells
-            .mapValues(v -> 1)
-            .filter(p -> p._2() >= minPts)
-            .join(allCells)
-            .mapValues(v -> v._2())
+            .mapValues(v -> 1)                      /* Emit pairs (cell, 1) */
+            .reduceByKey((v1, v2) -> v1 + v2)       /* Count points per cell */
+            .filter(p -> p._2() >= minPts)          /* Filter dense cells */
+            .join(allCells)                         /* Join with the original dataset */
+            .mapValues(v -> v._2())                 /* Drop the count value */
             .cache();
         
         return denseCells;
@@ -101,7 +105,7 @@ public class OutlierDetector implements Serializable {
      * @param allCells The PairRDD representing the 2D grid.
      * @param denseCells The PairRDD containing only the dense cells.
      * @param nonDenseCells The PairRDD containing only the non-dense cells.
-     * @return A PairRDD containing the list of core points for each cell.
+     * @return A PairRDD containing the core points for each cell.
      */
     private JavaPairRDD<Cell, Vector> findCoreCells(JavaPairRDD<Cell, Vector> allCells, JavaPairRDD<Cell, Vector> denseCells, JavaPairRDD<Cell, Vector> nonDenseCells) {
         /* List points to check for every cell */
@@ -111,12 +115,14 @@ public class OutlierDetector implements Serializable {
 
                 for (int i = p._1().getxPos() - 2; i <= p._1().getxPos() + 2; i++) {
                     for (int j = p._1().getyPos() - 2; j <= p._1().getyPos() + 2; j++) {
+                        /* Do not consider the diagonal cells */
                         if ((i == p._1().getxPos() - 2 && j == p._1().getyPos() - 2) || 
                             (i == p._1().getxPos() - 2 && j == p._1().getyPos() + 2) || 
                             (i == p._1().getxPos() + 2 && j == p._1().getyPos() - 2) ||
                             (i == p._1().getxPos() + 2 && j == p._1().getyPos() + 2))
                             continue;
                         
+                        /* Emit a pair (neighboring cell, point to be checked) */
                         tuples.add(new Tuple2<>(new Cell(i, j), p._2()));
                     }
                 }
@@ -126,14 +132,17 @@ public class OutlierDetector implements Serializable {
         
         /* Get core points from non-dense cells */
         JavaPairRDD<Cell, Vector> partiallyCoreCells = nonDenseCells
-            .join(pointsToCheck)
+            .join(pointsToCheck)                                        /* Join with the points to be checked */
             .mapToPair(p -> {
+                /* Check distance between points */
                 double d = distance(p._2()._1(), p._2()._2());
+
+                /* Emit a pair ((cell, point), distance < eps) */
                 return new Tuple2<>(new Tuple2<>(p._1(), p._2()._1()), d < eps ? 1 : 0);
             })
-            .reduceByKey((v1, v2) -> v1 + v2)
-            .filter(p -> p._2() >= minPts)
-            .mapToPair(p -> new Tuple2<>(p._1()._1(), p._1()._2()));
+            .reduceByKey((v1, v2) -> v1 + v2)                           /* Count points with distance < eps */
+            .filter(p -> p._2() >= minPts)                              /* Filter core points */
+            .mapToPair(p -> new Tuple2<>(p._1()._1(), p._1()._2()));    /* Emit a pair (cell, point) */
 
         return denseCells.union(partiallyCoreCells);
     }
@@ -142,7 +151,7 @@ public class OutlierDetector implements Serializable {
      * Returns the outliers.
      * 
      * @param coreCells The PairRDD containing only the core cells.
-     * @param nonCoreCells The PairRDD containin only the non-core cells.
+     * @param nonCoreCells The PairRDD containing only the non-core cells.
      * @return An RDD containing all the outliers.
      */
     private JavaRDD<Vector> findOutliers(JavaPairRDD<Cell, Vector> coreCells, JavaPairRDD<Cell, Vector> nonCoreCells) {
@@ -153,12 +162,14 @@ public class OutlierDetector implements Serializable {
 
                 for (int i = p._1().getxPos() - 2; i <= p._1().getxPos() + 2; i++) {
                     for (int j = p._1().getyPos() - 2; j <= p._1().getyPos() + 2; j++) {
+                        /* Do not consider the diagonal cells */
                         if ((i == p._1().getxPos() - 2 && j == p._1().getyPos() - 2) || 
                             (i == p._1().getxPos() - 2 && j == p._1().getyPos() + 2) || 
                             (i == p._1().getxPos() + 2 && j == p._1().getyPos() - 2) ||
                             (i == p._1().getxPos() + 2 && j == p._1().getyPos() + 2))
                             continue;
                         
+                        /* Emit a pair (neighboring cell, point to be checked) */
                         tuples.add(new Tuple2<>(new Cell(i, j), p._2()));
                     }
                 }
@@ -168,14 +179,17 @@ public class OutlierDetector implements Serializable {
         
         /* Get the list of outliers */
         JavaRDD<Vector> outliers = nonCoreCells
-            .leftOuterJoin(pointsToCheck)
+            .leftOuterJoin(pointsToCheck)                   /* Join with the points to be checked */
             .mapToPair(p -> {
-                boolean outlier = !p._2()._2().isPresent() || distance(p._2()._1(), p._2()._2().get()) >= eps;
-                return new Tuple2<>(new Tuple2<>(p._1(), p._2()._1()), outlier);
+                /* A point is an outlier if it has no neighbor or distance >= eps */
+                boolean o = !p._2()._2().isPresent() || distance(p._2()._1(), p._2()._2().get()) >= eps;
+
+                /* Emit a pair ((cell, point), outlier or not) */
+                return new Tuple2<>(new Tuple2<>(p._1(), p._2()._1()), o);
             })
-            .reduceByKey((v1, v2) -> v1 && v2)
-            .filter(p -> p._2())
-            .map(p -> p._1()._2());
+            .reduceByKey((v1, v2) -> v1 && v2)              /* Combine information from all points */
+            .filter(p -> p._2())                            /* Filter outliers */
+            .map(p -> p._1()._2());                         /* Map to the original vectors */
         
         return outliers;
     }
